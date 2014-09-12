a9bc1fe45767fc8175ba6c4dfc8bf9d7e03471c8
tgienger <tj.gienger@gmail.com>
2014-09-11 21:23:08 -0700

    some changes
    yep

--- crosshair/files/script.js
+++ crosshair/files/script.js
@@ -1,73 +1,119 @@
 angular.module('app', ['uiSlider'])
-.controller('crossController', function ($scope, $window) {
-  $scope.color = 'red'
-  $scope.width = 26
-  $scope.height = $scope.width
-  $scope.marginTop = -($scope.height / 2 )
-  $scope.marginLeft = -($scope.width / 2)
-  $scope.crossColor = '#ff0000'
-  $scope.dotColor = '#ff0000'
-  $scope.cStroke = '#ff0000'
-  $scope.cStrokeWidth = 10
-  $scope.pathStroke = $scope.crossColor
-  $scope.pathStrokeWidth = 10
-  $scope.outerThickness = 1.75
-  $scope.outerLength =  2.8
+.controller('crossController', function ($scope, $window, $timeout) {
+  $scope.color           = 'red';
+  $scope.width           = window.localStorage.crossWidth || 26;
+  $scope.height          = $scope.width;
+  $scope.marginTop       = -($scope.height / 2 );
+  $scope.marginLeft      = -($scope.width / 2);
+  $scope.crossColor      = window.localStorage.crossColor || '#ff0000';
+  $scope.dotColor        = window.localStorage.dotColor || '#ff0000';
+  $scope.outerLength     = window.localStorage.length || 2.8;
+  $scope.spinit          = window.localStorage.spinit || '';
+
+
+  // Startup fitting to screen resolution
+  // re-check every second for changes in resolution
+  var w;
+  var h;
+
+  (function fitScreen() {
+
+    var newW = window.screen.width;
+    var newH = window.screen.height;
+
+    if (w != newW) {
+
+      overwolf.windows.getCurrentWindow(function(result) {
+
+        if (result.status === 'success') {
+          overwolf.windows.changeSize(result.window.id, newW, newH, function () {});
+          overwolf.windows.changePosition(result.window.id, 0, 0, function () {});
+          w = newW;
+          h = newH;
+        }
+      });
+
+    }
+
+    $timeout(fitScreen, 1000);
+
+  })();
+
+
+
   // var win;
   $scope.close = function () {
-	overwolf.windows.getCurrentWindow(function (result) {
-      if (result.status == "success") {
-        overwolf.windows.close(result.window.id, function (result) {})
-      }
-    });
-  };
 
-  $scope.fitScreen = function () {
-	var newWidth = window.screen.width;
-	var newHeight = window.screen.height;
-    overwolf.windows.getCurrentWindow(function (result) {
-      if (result.status == "success") {
-		overwolf.windows.changeSize(result.window.id, newWidth, newHeight, function () {})
-      }
-    });
+  	overwolf.windows.getCurrentWindow(function (result) {
+        if (result.status == "success") {
+          overwolf.windows.close(result.window.id, function (result) {})
+        }
+      });
 
   };
 
-  var bigger = document.getElementById('biggerButton')
-  bigger.addEventListener('click', function (event) {
-    $scope.width += 1
-    $scope.height += 1
-    alert('width: ' + $scope.width + ' margin: ' + $scope.marginLeft)
-  })
 
-  var smaller = document.getElementById('smallerButton')
-  smaller.addEventListener('click', function (event) {
-    $scope.width -= 1
-    $scope.height -= 1
-  })
+
 
   $scope.setColor = function (color) {
+
     $scope.crossColor = color
-    $scope.cStroke = $scope.crossColor
-    $scope.pathStroke = $scope.crossColor
+
   }
-  
-  $scope.position = function () {
-	$scope.marginTop = -($scope.width / 2 + 1)
-	$scope.marginLeft = -($scope.width / 2 + 1)
-  }
-  
-  var spinning = false
+
+
+
+
+  var spinning = window.localStorage.spinning || false;
+  var rotated  = window.localStorage.rotated || false;
   $scope.spinner = function () {
-	if (!spinning) {
-		spinning = true
-		$scope.spinit = 'fa fa-spin'	
-	} else {
-		spinning = false
-		$scope.spinit = ''
-	}
+    rotated = false;
+  	if (!spinning) {
+  		spinning = true
+  		$scope.spinit = 'fa fa-spin'
+  	} else {
+  		spinning = false
+  		$scope.spinit = ''
+  	}
   }
-  
+
+
+
+  // rotate crosshair 45 deg
+  $scope.rotate = function() {
+    spinning = false;
+    if (!rotated) {
+      rotated = true;
+      $scope.spinit = 'fa-rotate-45';
+    } else {
+      rotated = false;
+      $scope.spinit = '';
+    }
+  }
+
+
+
+  // Center on resize
+  $scope.$watch($scope.width, function() {
+    $scope.marginTop  = -($scope.height / 2 );
+    $scope.marginLeft = -($scope.width / 2);
+  })
+
+
+
+  // save
+  $scope.save = function() {
+    window.localStorage.rotated    = rotated;
+    window.localStorage.spinning   = spinning;
+    window.localStorage.crossWidth = $scope.width;
+    window.localStorage.spinit     = $scope.spinit;
+    window.localStorage.dotColor   = $scope.dotColor;
+    window.localStorage.crossColor = $scope.crossColor;
+    window.localStorage.length     = $scope.outerLength;
+  }
+
+
+
   $scope.match = function () {
 	$scope.dotColor = $scope.crossColor;
   }
